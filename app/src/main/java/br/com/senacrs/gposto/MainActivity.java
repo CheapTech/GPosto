b7ce8c77d73f903cca5cdbfce3b36e226f4f5335
Tecinfo <inf1217n@gmail.com>
2019-08-12 20:42:11 -0300

    Teste Outro pc

--- app/src/main/java/br/com/senacrs/gposto/MainActivity.java
+++ app/src/main/java/br/com/senacrs/gposto/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 //test
     // \o/\o/
+    // te4stefsadgds
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
