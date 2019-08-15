693168ff0a29ecdbd1d15f0d5f50f7d4576bda9b
Tecinfo <@cheaptech1217n>
2019-08-15 19:56:20 -0300

    ALteração asdas

--- app/src/main/java/br/com/senacrs/gposto/MainActivity.java
+++ app/src/main/java/br/com/senacrs/gposto/MainActivity.java
@@ -5,9 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//test
-    // \o/\o/
-    // te4stefsadgds
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
