7dbb8d9af0696ddedacbebb1c91dc77fc4edea88
ADM <inf1217n@gmail.com>
2019-08-19 21:06:39 -0300

    Ajustando os Layouts(1º Versão)

--- app/src/main/java/br/com/senacrs/gposto/MainActivity.java
+++ app/src/main/java/br/com/senacrs/gposto/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //stdgfyfgh
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
