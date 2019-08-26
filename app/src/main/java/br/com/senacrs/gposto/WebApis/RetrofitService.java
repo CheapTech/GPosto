38f3f6efaedba26226043aa6eda170a69d16ee9c
Tecinfo <@cheaptech1217n>
2019-08-26 19:49:19 -0300

    ALteração asdas

--- app/src/main/java/br/com/senacrs/gposto/WebApis/RetrofitService.java
+++ app/src/main/java/br/com/senacrs/gposto/WebApis/RetrofitService.java
@@ -1,4 +1,17 @@
 package br.com.senacrs.gposto.WebApis;
 
+import java.util.List;
+
+import br.com.senacrs.gposto.LibClass.Combustivel;
+import br.com.senacrs.gposto.LibClass.TopPostos;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface RetrofitService {
+    @GET("CombustivelGet.php")
+    Call<List<Combustivel>> getListCombustivel();
+
+    @GET("gasolinaTeste.php")
+    Call<List<TopPostos>> getListTopPostos(@Query(value = "id") int id);
 }
