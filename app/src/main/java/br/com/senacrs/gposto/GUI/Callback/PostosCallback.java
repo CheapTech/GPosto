ffbf0b7cc6fad18318fce914ff755aea59a28e3d
Tecinfo <inf1217n@gmail.com>
2019-09-03 21:03:04 -0300

    Postos Controller/Deserializer/Callback

---
+++ app/src/main/java/br/com/senacrs/gposto/GUI/Callback/PostosCallback.java
@@ -0,0 +1,4 @@
+package br.com.senacrs.gposto.GUI.Callback;
+
+public interface PostosCallback {
+}
---
+++ app/src/main/java/br/com/senacrs/gposto/Utilities/Deserializer/PostosDeserializer.java
@@ -0,0 +1,4 @@
+package br.com.senacrs.gposto.Utilities.Deserializer;
+
+public class PostosDeserializer {
+}
